6c9ad5679db791cc059d721ef3a265f6a2b4e08c
Samuel Gallagher <segallagher@alaska.edu>
2025-09-02 11:16:49 -0800

    feat: add generation of dataset

---
+++ generate_dataset.py
@@ -0,0 +1,51 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
+import shutil
+from pathlib import Path
+
+## PARAMETERS
+data_path = Path("./data")
+dataset_name = "dataset.csv"
+
+# initialize dataset
+dataset: pd.DataFrame = pd.DataFrame(columns=[
+    "authors",
+    "journal_entry",
+    "pdf_url",
+    "publish_date",
+    "title",
+    "abstract",
+    ])
+
+# initialize kaggle api
+api = KaggleApi()
+api.authenticate()
+
+# download dataset "Arxiv.org AI Research Papers Dataset"
+api.dataset_download_files(
+    dataset="yasirabdaali/arxivorg-ai-research-papers-dataset",
+    path=data_path / "data",
+    unzip=True,
+)
+
+# load data
+data: pd.DataFrame = pd.read_csv(data_path / "data" / "arxiv_ai.csv")
+# delete downloaded file
+shutil.rmtree(data_path / "data")
+
+# format data
+data.drop(columns=["categories", "comment", "doi", "journal_ref", "primary_category", "updated"], inplace=True)
+data.rename(
+        columns={
+            "entry_id": "journal_entry",
+            "published": "publish_date",
+            "summary": "abstract",
+        },
+        inplace=True
+    )
+
+# add data to dataset
+dataset = pd.concat([dataset, data], axis=0, ignore_index=True)
+
+# write to file
+dataset.to_csv(data_path / dataset_name)
